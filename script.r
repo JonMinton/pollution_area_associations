ec44f164e01abfe6a8c1672d5be965597baeade2
Jonathan Minton <nate.minton@gmail.com>
2015-01-22 11:58:42 +0000

    add skeleton structure to project folder

---
+++ script.r
@@ -0,0 +1,6 @@
+# 22/1/2015
+
+# Script to explore ecological associations between pollution estimated at datazone level 
+# and socioeconomic characteristics of neighbourhoods 
+
+# n.b. using packrat to manage dependencies 
